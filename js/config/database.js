69008d54f2e1b8b3c32e1edc808914c3a194a85d
yuebaiXu <xyb1114@gmail.com>
2016-06-23 20:05:24 +0200

    second

---
+++ js/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	//'url':'';//here please add your mongodb configuration's url
+	'url' : 'mongodb://albert:albertwhite@ds039850.mlab.com:39850/users' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+}
